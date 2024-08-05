76d809cc2385ab402a63837538f157996ca4dda8
Jonathan <jargumedo.aj@gmail.com>
2024-08-04 22:09:34 -0600

    Finished the buttons and screen opacity section

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity} from "react-native";
 
+/*
+TouchableOpacity allows you to wrap any component inside it. This is great for clicking on 
+images, texts, etc. It's the proper way to make users clisk on the screen, the button 
+component is only used for a 'hot fix'.
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+*/
+const HomeScreen = ({navigation}) => {
+  return(
+  <View>
+    <Text style={styles.text}>HomeScreen</Text>
+    <Button 
+      title="Go to Components Screen" 
+      onPress={() => navigation.navigate('Components')}
+    />
+    <Button 
+      title="Go to List Screen" 
+      onPress={() => navigation.navigate('List')}
+    />
+    
+    {/* <TouchableOpacity onPress={() => console.log("List Screen")}>
+      <Text>List Screen</Text>
+    </TouchableOpacity> */}
+  </View>
+  );
 };
 
 const styles = StyleSheet.create({
